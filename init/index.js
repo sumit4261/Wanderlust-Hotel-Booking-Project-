3acfc178507c9279eb51a202a04382b41c989393
Sumit Mane <sumitmane8071@gmail.com>
2025-06-27 18:57:21 +0530

    Update DB connection to use Atlas env variable for deployment

--- init/index.js
+++ init/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../Models/listing.js");
